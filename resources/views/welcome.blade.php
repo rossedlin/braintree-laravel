85e3cb73823969929df96b0cd38028a7f2fdef84
Ross Edlin <rossedlin@gmail.com>
2024-03-18 16:15:39 +0000

    Fix title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Chat Laravel Pusher | Edlin App</title>
+  <title>Braintree Laravel | Edlin App</title>
   <link rel="icon" href="https://assets.edlin.app/favicon/favicon.ico"/>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
