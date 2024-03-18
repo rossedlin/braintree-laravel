cee1335b616372c3f46f061078d340bc02513701
Ross Edlin <rossedlin@gmail.com>
2024-03-18 16:09:23 +0000

    Working

--- config/braintree.php
+++ config/braintree.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    'environment' => env('BRAINTREE_ENVIRONMENT'),
-    'merchantId'  => env('BRAINTREE_MERCHANT_ID'),
-    'publicKey'   => env('BRAINTREE_PUBLIC_KEY'),
-    'privateKey'  => env('BRAINTREE_PRIVATE_KEY'),
+    'environment'     => env('BRAINTREE_ENVIRONMENT'),
+    'tokenizationKey' => env('BRAINTREE_TOKENIZATION_KEY'),
+    'merchantId'      => env('BRAINTREE_MERCHANT_ID'),
+    'publicKey'       => env('BRAINTREE_PUBLIC_KEY'),
+    'privateKey'      => env('BRAINTREE_PRIVATE_KEY'),
 ];
